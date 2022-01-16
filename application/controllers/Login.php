ecfe35ec7bfae7e363e8bec7666a7aeae196f3f0
Rizki36 <cahrizki.rf@gmail.com>
2022-01-16 09:36:41 +0700

    feat : login admin

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -71,4 +71,45 @@ class login extends CI_Controller
 
 		setresponse(200, ['msg' => 'Berhasil login']);
 	}
+
+	function admin()
+	{
+		## return view when post empty
+		if (!$this->input->post()) {
+			$this->load->view('layout', [
+				'use_nav' => false,
+				'main' => $this->load->view('v_login', [], true),
+				'use_footer' => false,
+				'scripts' => $this->load->view('v_login_scripts', [
+					'is_admin' => true
+				], true)
+			]);
+
+			return;
+		}
+
+
+		## validation rule
+		$this->form_validation->set_rules('username', 'Username', 'trim|required|min_length[5]');
+		$this->form_validation->set_rules('password', 'Password', 'trim|required|min_length[5]');
+
+		## validation error
+		if (!$this->form_validation->run()) setresponse(400, [
+			'validation' => $this->form_validation->error_array()
+		]);
+
+		$u = filter_xss($this->input->post('username'));
+		$p = Auth::enc(filter_xss($this->input->post('password')));
+
+		$waktu = time() + 25200;
+		$expired = 30000;
+
+		## set session
+		$this->session->set_userdata([
+			'role' => User_Role::admin,
+			'timeout' => $waktu + $expired
+		]);
+
+		setresponse(200, ['msg' => 'Berhasil login']);
+	}
 }
