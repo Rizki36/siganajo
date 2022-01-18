87a3cce67c684f26477ed9334749890d384a182b
Rizki36 <cahrizki.rf@gmail.com>
2022-01-18 19:39:18 +0700

    feat : form penggeledahan

---
+++ application/controllers/Penggeledahan.php
@@ -0,0 +1,221 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class penggeledahan extends CI_Controller
+{
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->helper('form');
+		$this->load->library('form_validation');
+		$this->load->model('M_Penggeledahan');
+	}
+
+	public function index()
+	{
+		$form1['nama_penyidik'] = ['class_container' => 'col-6', 'type' => 'text', 'label' => 'Nama Penyidik'];
+		$form1['nip_nrp'] = ['class_container' => 'col-6', 'type' => 'text', 'label' => 'NIP/NRP'];
+		$form1['nomor_telepon_wa'] = ['class_container' => 'col-6', 'type' => 'text', 'label' => 'Nomor Telepon Wa'];
+		$form1['email'] = ['class_container' => 'col-6', 'type' => 'text', 'label' => 'Email'];
+
+		$optionsPolres = [
+			['value' => '', 'label' => 'Pilih Polres Pengaju'],
+			['value' => 'Polres Sat Reskrim', 'label' => 'Polres Sat Reskrim'],
+			['value' => 'Polres Sat Narkoba', 'label' => 'Polres Sat Narkoba'],
+			['value' => 'Polres Sat Lantas', 'label' => 'Polres Sat Lantas'],
+			['value' => 'Polsek Jombang Kota', 'label' => 'Polsek Jombang Kota'],
+			['value' => 'Polsek Ploso', 'label' => 'Polsek Ploso'],
+			['value' => 'Polsek Tembelang', 'label' => 'Polsek Tembelang'],
+			['value' => 'Polsek Plandaan', 'label' => 'Polsek Plandaan'],
+			['value' => 'Polsek Mojoagung', 'label' => 'Polsek Mojoagung'],
+			['value' => 'Polsek Kesamben', 'label' => 'Polsek Kesamben'],
+			['value' => 'Polsek Ngoro', 'label' => 'Polsek Ngoro'],
+			['value' => 'Polsek Ngusikan', 'label' => 'Polsek Ngusikan'],
+			['value' => 'Polsek Sumobito', 'label' => 'Polsek Sumobito'],
+			['value' => 'Polsek Bandar Kedung Mulyo', 'label' => 'Polsek Bandar Kedung Mulyo'],
+			['value' => 'Polsek Peterongan', 'label' => 'Polsek Peterongan'],
+			['value' => 'Polsek Kabuh', 'label' => 'Polsek Kabuh'],
+			['value' => 'Polsek Kudu', 'label' => 'Polsek Kudu'],
+			['value' => 'Polsek Bareng', 'label' => 'Polsek Bareng'],
+			['value' => 'Polsek Diwek', 'label' => 'Polsek Diwek'],
+			['value' => 'Polsek Jogoroto', 'label' => 'Polsek Jogoroto'],
+			['value' => 'Polsek Megaluh', 'label' => 'Polsek Megaluh'],
+			['value' => 'Polsek Gudo', 'label' => 'Polsek Gudo'],
+			['value' => 'Posek Perak', 'label' => 'Posek Perak'],
+			['value' => 'Mabes Polri/Polda/Kejaksaan/Penyidik PNS', 'label' => 'Mabes Polri/Polda/Kejaksaan/Penyidik PNS'],
+			['value' => 'Lain-lain', 'label' => 'Lain-lain'],
+		];
+
+		$form1['polres_polsek_pengaju'] = ['class_container' => 'col-6', 'type' => 'select', 'options' => $optionsPolres, 'label' => 'Polres Polsek Pengaju'];
+
+		$optionsJenis = [
+			['value' => '', 'label' => 'Pilih Jenis Permohonan'],
+			['value' => 'Izin Penggeledahan', 'label' => 'Izin Penggeledahan'],
+			['value' => 'Persetujuan Penggeledahan', 'label' => 'Persetujuan Penggeledahan']
+		];
+
+		$form1['jenis_permohonan'] = [
+			'class_container' => 'col-6', 'type' => 'select', 'options' => $optionsJenis, 'label' => 'Jenis Permohonan',
+			'attr' => [
+				'x-model' => 'jenis_permohonan'
+			]
+		];
+
+		$steps['penyidik']['forms']['first']['input'] = $form1;
+		$steps['penyidik']['forms']['first']['template_attr'] = ['x-if' => 'true'];
+		$steps['penyidik']['validation_link'] = base_url('penggeledahan/validation_penyidik');
+		$steps['penyidik']['name'] = 'Data Penyidik';
+
+		$step2Form1['surat_permohonan_dari_penyidik'] = ['class_container' => 'col-12', 'type' => 'file', 'label' => 'Surat Permohonan Dari Penyidik', 'attr' => ['accept' => ".pdf", 'required']];
+		$step2Form1['surat_perintah_penggeledahan'] = ['class_container' => 'col-12', 'type' => 'file', 'label' => 'Surat Perintah Penggeledahan', 'attr' => ['accept' => ".pdf", 'required']];
+		$step2Form1['ba_penggeledahan'] = ['class_container' => 'col-12', 'type' => 'file', 'label' => 'BA Penggeledahan', 'attr' => ['accept' => ".pdf", 'required']];
+		$step2Form1['surat_pemberitahuan_dimulainya_penyidikan'] = ['class_container' => 'col-12', 'type' => 'file', 'label' => 'Surat Pemberitahuan Dimulainya Penyidikan', 'attr' => ['accept' => ".pdf", 'required']];
+		$step2Form1['resume_singkat'] = ['class_container' => 'col-12', 'type' => 'file', 'label' => 'Resume Singkat', 'attr' => ['accept' => ".pdf",]];
+		$step2Form1['jenis_permohonan_'] = ['class_container' => 'd-none', 'type' => 'hidden', 'label' => ''];
+
+		$step2Form2['surat_permohonan_dari_penyidik'] = ['class_container' => 'col-12', 'class' => '', 'type' => 'file', 'label' => 'Surat Permohonan Dari Penyidik', 'attr' => ['accept' => ".pdf", 'required']];
+		$step2Form2['surat_perintah_penggeledahan'] = ['class_container' => 'col-12', 'class' => '', 'type' => 'file', 'label' => 'Surat Perintah Penggeledahan', 'attr' => ['accept' => ".pdf", 'required']];
+		$step2Form2['laporan_polisi'] = ['class_container' => 'col-12', 'class' => '', 'type' => 'file', 'label' => 'Laporan Polisi', 'attr' => ['accept' => ".pdf", 'required']];
+		$step2Form2['surat_pemberitahuan_dimulainya_penyidikan'] = ['class_container' => 'col-12', 'class' => '', 'type' => 'file', 'label' => 'Surat Pemberitahuan Dimulainya Penyidikan', 'attr' => ['accept' => ".pdf", 'required']];
+		$step2Form2['ba_penggeledahan'] = ['class_container' => 'col-12', 'class' => '', 'type' => 'file', 'label' => 'BA Penggeledahan', 'attr' => ['accept' => ".pdf", 'required']];
+		$step2Form2['surat_tanda_terima_barang_bukti'] = ['class_container' => 'col-12', 'class' => '', 'type' => 'file', 'label' => 'Surat Tanda Terima Barang Bukti', 'attr' => ['accept' => ".pdf", 'required']];
+		$step2Form2['surat_perintah_penyidik'] = ['class_container' => 'col-12', 'class' => '', 'type' => 'file', 'label' => 'Surat Perintah Penyidik', 'attr' => ['accept' => ".pdf"]];
+		$step2Form2['resume_singkat'] = ['class_container' => 'col-12', 'class' => '', 'type' => 'file', 'label' => 'Resume Singkat', 'attr' => ['accept' => ".pdf"]];
+		$step2Form2['jenis_permohonan_'] = ['class_container' => 'd-none', 'type' => 'hidden', 'label' => ''];
+
+		$steps['berkas']['forms']['berkas1']['input'] = $step2Form1;
+		$steps['berkas']['forms']['berkas1']['template_attr'] = ['x-if' => "jenis_permohonan === 'Izin Penggeledahan'"];
+		$steps['berkas']['forms']['berkas2']['input'] = $step2Form2;
+		$steps['berkas']['forms']['berkas2']['template_attr'] = ['x-if' => "jenis_permohonan === 'Persetujuan Penggeledahan'"];
+		$steps['berkas']['validation_link'] = base_url('penggeledahan/validation_berkas');
+		$steps['berkas']['name'] = 'Data Berkas';
+
+		$this->load->view('layout', [
+			'main' => $this->load->view(
+				'v_form',
+				[
+					'header_with_bg' => true,
+					'steps' => $steps,
+					'data_alpine' => [
+						'jenis_permohonan' => ''
+					]
+				],
+				true
+			),
+			'scripts' => $this->load->view('v_form_scripts', [], true)
+		]);
+	}
+
+	public function validation_penyidik()
+	{
+		## validation rule
+		$this->form_validation->set_rules('nama_penyidik', 'Nama Penyidik', 'trim|required|min_length[3]');
+		$this->form_validation->set_rules('nip_nrp', 'NIP/NRP', 'trim|required|min_length[3]');
+		$this->form_validation->set_rules('nomor_telepon_wa', 'Nomor Telepon WA', 'trim|required|min_length[3]');
+		$this->form_validation->set_rules('email', 'Email', 'trim|required|min_length[3]');
+		$this->form_validation->set_rules('polres_polsek_pengaju', 'Polres Polsek Pengaju', 'trim|required');
+		$this->form_validation->set_rules('jenis_permohonan', 'Jenis Permohonan', 'trim|required');
+
+		## validatoon error
+		if (!$this->form_validation->run()) {
+			setresponse(400, [
+				'validation' => $this->form_validation->error_array()
+			]);
+		}
+		setresponse(200, []);
+	}
+
+	public function validation_berkas()
+	{
+		## validation rule
+		$this->form_validation->set_rules('nama_penyidik', 'Nama Penyidik', 'trim|required|min_length[3]');
+		$this->form_validation->set_rules('nip_nrp', 'NIP/NRP', 'trim|required|min_length[3]');
+		$this->form_validation->set_rules('nomor_telepon_wa', 'Nomor Telepon WA', 'trim|required|min_length[3]');
+		$this->form_validation->set_rules('email', 'Email', 'trim|required|min_length[3]');
+		$this->form_validation->set_rules('polres_polsek_pengaju', 'Polres Polsek Pengaju', 'trim|required');
+		$this->form_validation->set_rules('jenis_permohonan', 'Jenis Permohonan', 'trim|required');
+
+		## validatoon error
+		if (!$this->form_validation->run()) setresponse(400, [
+			'validation' => $this->form_validation->error_array()
+		]);
+
+		## upload file
+		if ($_POST['jenis_permohonan'] === 'Izin Penggeledahan') {
+			$inpNames = [
+				'surat_permohonan_dari_penyidik',
+				'surat_perintah_penggeledahan',
+				'ba_penggeledahan',
+				'surat_pemberitahuan_dimulainya_penyidikan',
+				'resume_singkat',
+			];
+		} else {
+			$inpNames = [
+				'surat_permohonan_dari_penyidik',
+				'surat_perintah_penggeledahan',
+				'laporan_polisi',
+				'surat_pemberitahuan_dimulainya_penyidikan',
+				'ba_penggeledahan',
+				'surat_tanda_terima_barang_bukti',
+				'surat_perintah_penyidik',
+				'resume_singkat',
+			];
+		}
+
+		$file_json = [];
+		$time = time();
+		foreach ($inpNames as $inpName) {
+			try {
+				$filename = MyFiles::upload($inpName, $time . '_' . @$_POST['username'] . '_' . $inpName, MyFiles::$penggeledahan);
+				$file_json[$inpName] = $filename;
+			} catch (\Throwable $th) {
+				if ($inpName !== 'resume_singkat') setresponse(400, ['msg' => M_Penggeledahan::get_label($inpName) . ' : ' . $th->getMessage()]);
+			}
+		}
+
+		$data = [
+			'user_id' => @$_SESSION['user_id'],
+			'nama_penyidik' => filter_xss($this->input->post('nama_penyidik')),
+			'nip_nrp' => filter_xss($this->input->post('nip_nrp')),
+			'nomor_telepon_wa' => filter_xss($this->input->post('nomor_telepon_wa')),
+			'email' => filter_xss($this->input->post('email')),
+			'polres_polsek_pengaju' => filter_xss($this->input->post('polres_polsek_pengaju')),
+			'jenis_permohonan' => filter_xss($this->input->post('jenis_permohonan')),
+			'files_json' => json_encode($file_json)
+		];
+
+
+		$m_penggeledahan = new M_Penggeledahan();
+		$m_penggeledahan->insert($data);
+
+
+		$enc_id = base64_encode($this->db->insert_id());
+		$this->load->library('MyEmail');
+
+		unset($data['user_id']);
+		unset($data['files_json']);
+
+		$body = $this->load->view('emails/v_penggeledahan', [
+			'title' => 'Form Baru',
+			'text' => 'Pastikan login terlebih dahulu untuk mengakses link dibawah.',
+			'data' => $data,
+			'files' => $file_json,
+			'path' => MyFiles::$penggeledahan,
+			'is_admin' => true,
+			'link' => base_url('admin/penggeledahan/print/' . $enc_id)
+		], true);
+		MyEmail::send('sigenajo.pn.jombang@gmail.com', 'Form baru', $body);
+		$body = $this->load->view('emails/v_penggeledahan', [
+			'title' => 'Form Baru',
+			'text' => 'Pastikan login terlebih dahulu untuk mengakses link dibawah.',
+			'data' => $data,
+			'files' => $file_json,
+			'path' => MyFiles::$penggeledahan,
+			'is_admin' => false,
+			'link' => base_url('admin/penggeledahan/print/' . $enc_id)
+		], true);
+		MyEmail::send($data['email'], 'Salinan Form', $body);
+
+		setresponse(200, $data);
+	}
+}
---
+++ application/models/M_Penggeledahan.php
@@ -0,0 +1,100 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+/**
+ * M_Penggeledahan
+ */
+class M_Penggeledahan extends MY_Model
+{
+	private $table = 'penggeledahan';
+
+	public function __construct()
+	{
+		parent::__construct($this->table);
+	}
+
+	public static function get_label($key)
+	{
+		switch ($key) {
+			case 'nama_penyidik':
+				return 'Nama Penyidik';
+			case 'nip_nrp':
+				return 'NIP/NRP';
+			case 'nomor_telepon_wa':
+				return 'Nomor Telepon WA';
+			case 'email':
+				return 'Email';
+			case 'polres_polsek_pengaju':
+				return 'Polres Polsek Pengaju';
+			case 'jenis_permohonan':
+				return 'Jenis Permohonan';
+
+			case 'surat_permohonan_dari_penyidik':
+				return 'Surat Permohonan Dari Penyidik / PPNS';
+			case 'surat_perintah_penggeledahan':
+				return 'Surat Perintah Penggeledahan';
+			case 'laporan_polisi':
+				return 'Laporan Polisi';
+			case 'surat_pemberitahuan_dimulainya_penyidikan':
+				return 'Surat Pemberitahuan Dimulainya Penyidikan (SPDP)';
+			case 'ba_penggeledahan':
+				return 'BA Penggeledahan';
+			case 'surat_tanda_terima_barang_bukti':
+				return 'Surat Tanda Terima Barang Bukti';
+			case 'surat_perintah_penyidik':
+				return 'Surat Perintah Penyidik';
+			case 'resume_singkat':
+				return 'Resume Singkat';
+			default:
+				return $key;
+		}
+	}
+}
+
+
+
+/**
+ * Penyitaan
+ * to convert data from M_Penyitaan
+ * @property int $id_penggeledahan
+ * @property int $user_id
+ * @property string $nama_penyidik
+ * @property string $nip_nrp
+ * @property string $nomor_telepon_wa
+ * @property string $email
+ * @property string $polres_polsek_pengaju
+ * @property string $jenis_permohonan
+ * @property array $files_json
+ * @property int $created_at
+ * @property bool $is_dibaca
+ * 
+ */
+class Penggeledahan_DTO
+{
+	public $id_penggeledahan;
+	public $user_id;
+	public $nama_penyidik;
+	public $nip_nrp;
+	public $nomor_telepon_wa;
+	public $email;
+	public $polres_polsek_pengaju;
+	public $jenis_permohonan;
+	public $files_json;
+	public $created_at;
+	public $is_dibaca;
+
+	public function __construct($data)
+	{
+		$this->id_penggeledahan = @$data->id_penggeledahan;
+		$this->user_id = @$data->user_id;
+		$this->nama_penyidik = @$data->nama_penyidik;
+		$this->nip_nrp = @$data->nip_nrp;
+		$this->nomor_telepon_wa = @$data->nomor_telepon_wa;
+		$this->email = @$data->email;
+		$this->polres_polsek_pengaju = @$data->polres_polsek_pengaju;
+		$this->jenis_permohonan = @$data->jenis_permohonan;
+		$this->files_json = json_decode(@$data->files_json, true);
+		$this->created_at = strtotime(@$data->created_at);
+		$this->is_dibaca = (int)@$data->is_dibaca === 1;
+	}
+}
